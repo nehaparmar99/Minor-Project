559e597dd472de99805657dcbf528e43f0ddf1b4
Neha Parmar <nehaparmar1899@gmail.com>
2021-04-07 13:28:51 +0530

    model server

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask_cors import CORS
+from flask import Flask, request, render_template, json, jsonify, send_from_directory
+import json
+import cv2
+import numpy as np
+import io
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/model')
+def model():
+    json_data = json.load(open("model/facemo.json"))
+    return jsonify(json_data)
+
+
+@app.route('/<path:path>')
+def load_shards(path):
+    return send_from_directory('model', path)
+
+
+if __name__ == "__main__":
+    app.run()
